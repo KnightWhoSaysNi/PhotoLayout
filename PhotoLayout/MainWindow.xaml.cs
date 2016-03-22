d99f497d79d2b4b282773bd4a2b288101359660f
KnightWhoSaysNi <nnikola.milovanovic@gmail.com>
2016-03-22 05:05:02 +0100

    MainWindow code cleaned, temp LoadPhotos with async logic added

--- PhotoLayout/MainWindow.xaml.cs
+++ PhotoLayout/MainWindow.xaml.cs
@@ -30,7 +30,7 @@ namespace PhotoLayout
     {
         #region - Fields -
         
-        private ImageSource image;
+        //private ImageSource image;
 
         #endregion
 
@@ -43,7 +43,7 @@ namespace PhotoLayout
             
             this.CommandBindings.Add(new CommandBinding(ApplicationCommands.Open, OnOpen));
 
-            AllPhotos = new ObservableCollection<BitmapImage>();
+            AllPhotos = new ObservableCollection<BitmapSource>();
         }
 
         #endregion  
@@ -63,23 +63,25 @@ namespace PhotoLayout
 
         #region - Properties -
 
-        public ImageSource Image
-        {
-            get { return image; }
-            set
-            {
-                this.image = value;
-                OnPropertyChanged();
-            }
-        }
-        public ObservableCollection<BitmapImage> AllPhotos { get; set; }
+        //public ImageSource Image
+        //{
+        //    get { return image; }
+        //    set
+        //    {
+        //        this.image = value;
+        //        OnPropertyChanged();
+        //    }
+        //}
+
+        public ObservableCollection<BitmapSource> AllPhotos { get; set; }
+        public List<BitmapImage> StoredPhotos { get; set; }
 
         #endregion  
 
         private void OnOpen(object sender, ExecutedRoutedEventArgs e)
         {
             // TODO Check if this releases memory!!! --- it does not without direct call to GC
-            AllPhotos.Clear();
+            //AllPhotos.Clear();
 
             var openDialog = new OpenFileDialog();
             openDialog.Filter = "Image files (*.BMP;*.JPG;*.GIF)|*.BMP;*.JPG;*.GIF";
@@ -89,15 +91,23 @@ namespace PhotoLayout
             {
                 var imagePaths = openDialog.FileNames;
 
-                // Just creates a list of bitmap images
+                //// Just creates a list of bitmap images
                 //List<BitmapImage> bitmaps = new List<BitmapImage>();
                 //foreach (var imagePath in imagePaths)
                 //{
                 //    var count = bitmaps.Count;
-                //    bitmaps.Add(CreateImage(imagePath));                    
+                //    bitmaps.Add(CreateImage(imagePath));
                 //}
-
-                //bitmaps.Clear();
+                //if (StoredPhotos == null)
+                //{
+                //    StoredPhotos = new List<BitmapImage>(bitmaps);
+                //}
+                //else
+                //{
+                //    StoredPhotos.AddRange(bitmaps);
+                //}
+                //return;
+                
 
                 // TODO Is this the best way of updating UI?
                 var worker = new BackgroundWorker();
@@ -106,7 +116,7 @@ namespace PhotoLayout
                 {
                     foreach (var imagePath in imagePaths)
                     {
-                        BitmapImage bitmap = CreateImage(imagePath);
+                        BitmapSource bitmap = CreateImage(imagePath);
                         Dispatcher.BeginInvoke((Action)(() => AllPhotos.Add(bitmap)), DispatcherPriority.Background);
                     }
                 };
@@ -136,21 +146,33 @@ namespace PhotoLayout
             GC.Collect();
         }
 
-        private BitmapImage CreateImage(string filePath)
+        // ***************************************************************
+        // ******** NOT INTENDED FOR DIRECT LOADING OF PHOTOS ************
+        // ******** SET TO ONLOAD FOR DIRECT LOAD, NONE OTHERWISE ********
+        // ***************************************************************
+        private BitmapSource CreateImage(string filePath)
         {
+            //var source = new BitmapImage();
+            //using (Stream stream = new FileStream(filePath, FileMode.Open))
+            //{
+            //    source.BeginInit();
+            //    source.StreamSource = stream;
+            //    source.CacheOption = BitmapCacheOption.OnLoad; // intentionally OnLoad
+            //    source.EndInit();
+            //    source.Freeze(); // TODO Check if this is necessary
+            //}
+            //return source;
             
             BitmapImage bitmapImage = new BitmapImage();
-            //var stream = File.OpenRead(filePath);
 
             bitmapImage.BeginInit();
             //bitmapImage.CreateOptions = BitmapCreateOptions.DelayCreation;
             bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
 
             // If this is not set memory is not being released at all, until AllPhotos is cleared AND GC is called. 
-            // But with this set memory is being released automatically, probably after AllPhotos has been filled up
-            bitmapImage.DecodePixelWidth = 200;
+            // But with this set to a low value, memory is being released automatically, maybe after AllPhotos has been filled up
+            bitmapImage.DecodePixelWidth = 2000;
             bitmapImage.UriSource = new Uri(filePath, UriKind.RelativeOrAbsolute);
-            //bitmapImage.StreamSource = stream;
             // To save significant application memory, set the DecodePixelWidth or  
             // DecodePixelHeight of the BitmapImage value of the image source to the desired 
             // height or width of the rendered image. If you don't do this, the application will 
@@ -163,33 +185,41 @@ namespace PhotoLayout
             bitmapImage.EndInit();
             bitmapImage.Freeze(); // TODO Check if the images can be resized after this
 
-            //stream.Dispose();
-            //bitmapImage = null;
-
-            //imagePreview.Source = bitmapImage;
             return bitmapImage;
         }
 
-        private void NextImage(object sender, RoutedEventArgs e)
+        #region - Temp test methods -
+
+        private void ClearStoredPhotos(object sender, RoutedEventArgs e)
         {
-            
+            if (AllPhotos != null)
+            {
+                AllPhotos.Clear();
+                return;
+            }
+
+            if (StoredPhotos != null)
+            {
+                StoredPhotos.Clear();
+            }
+
+            if (AllPhotos.Count > 0)
+            {
+                AllPhotos.RemoveAt(0);
+            }
         }
 
-        private void PreviousImage(object sender, RoutedEventArgs e)
+        private int count = 0;
+        private void PreviewLastPhoto(object sender, RoutedEventArgs e)
         {
-            //if (AllPhotos == null || AllPhotos.Count == 0)
-            //{
-            //    System.Diagnostics.Debug.WriteLine("There are no images");
-            //    return;
-            //}
-
-            //if (currentImageIndex == 0)
-            //{
-            //    currentImageIndex = AllPhotos.Count;
-            //}
-            //currentImageIndex--;
-
-            //CreateImage(AllPhotos[currentImageIndex]);
+            AllPhotos.Add(StoredPhotos[count]);
+            count++;
+            if (count == StoredPhotos.Count)
+            {
+                count = 0;
+            }
         }
+
+        #endregion
     }
 }
