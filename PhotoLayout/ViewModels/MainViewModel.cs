fa34fd4e130f9e8c1b8813e07cb6e101cef841b1
KnightWhoSaysNi <nnikola.milovanovic@gmail.com>
2016-03-22 19:57:35 +0100

    ViewModels added

---
+++ PhotoLayout/ViewModels/MainViewModel.cs
@@ -0,0 +1,35 @@
+﻿using PhotoLayout.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace PhotoLayout.ViewModels
+{
+    public class MainViewModel : BindableBase
+    {
+        #region - Fields -
+
+
+
+        #endregion
+
+        #region - Contructors - 
+
+        public MainViewModel()
+        {
+            TestVM = new TestViewModel();
+        }
+
+        #endregion
+
+        #region - Properties -
+
+        public TestViewModel TestVM { get; private set; }
+
+        #endregion
+    }
+}
---
+++ PhotoLayout/ViewModels/TestViewModel.cs
@@ -0,0 +1,21 @@
+﻿using PhotoLayout.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace PhotoLayout.ViewModels
+{
+    public class TestViewModel : BindableBase
+    {
+        public TestViewModel()
+        {
+            TestCommand = new RelayCommand(x => MessageBox.Show("It's working"));
+        }
+
+        public ICommand TestCommand { get; set; }
+    }
+}
