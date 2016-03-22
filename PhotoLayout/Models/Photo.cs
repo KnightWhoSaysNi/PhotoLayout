c83095fbdece01c2b40575eb94b10506ee26643b
Nikola Milovanovic <nikola.milovanovic@easygoing.company>
2016-03-22 18:15:55 +0100

    Added Helpers\RelayCommand and Models\Photo classes

---
+++ PhotoLayout/Models/Photo.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace PhotoLayout.Models
+{
+    public class Photo : INotifyPropertyChanged
+    {
+        #region - Fields -
+
+        private bool isSaved;
+
+        #endregion
+
+        #region - Constructors -
+
+        public Photo()
+        {
+
+        }
+
+        #endregion
+
+        #region - Events -
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        #endregion
+
+        #region - Properties -
+
+        // TODO !!! Check if Drawing image is a better solution than an Image control with BitmapSource !!!
+        // Might need refactoring later on
+        public BitmapSource Source { get; set; }
+        public BitmapSource ThumbnailSource { get; set; }
+
+        public string Name { get; set; }
+        
+        public bool IsSaved
+        {
+            get { return isSaved; }
+            set
+            {
+                if (isSaved != value)
+                {
+                    isSaved = value;
+                    OnPropertyChanged(nameof(IsSaved));
+                }
+            }
+        }
+
+        #endregion
+
+        #region - Public methods -
+
+        public override string ToString()
+        {
+            return this.Name;
+        }
+
+        #endregion
+    }
+}
