19db4db547537005022f5d18acd4326f9f91a701
Nikola Milovanovic <nikola.milovanovic@easygoing.company>
2016-04-08 14:34:44 +0200

    Photo.UpdateBitmapSources renamed to RefreshBitmapSources
    Working on DirectoryExplorer: Models\Folder.cs added; MainWindow.xaml.cs changed; .sln removed from gitignore

---
+++ PhotoLayout/Converters/ObjectToBitmapSourceConverter.cs
@@ -0,0 +1,30 @@
+﻿using PhotoLayout.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace PhotoLayout.Converters
+{
+    // TODO Delete this if it's not necessary
+    public class ObjectToBitmapSourceConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value is Folder)
+            {
+
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+    }
+}
