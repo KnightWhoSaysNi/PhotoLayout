ae97563acafc9a6d08e33238405f42e199b37759
Nikola Milovanovic <nikola.milovanovic@easygoing.company>
2016-04-15 16:39:28 +0200

    AttachedProperties.cs added with SelectedItems property for ListBox

---
+++ PhotoLayout/Helpers/AttachedProperties.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace PhotoLayout.Helpers
+{
+    public class AttachedProperties
+    {
+        #region - ListBox SelectedItems -
+
+        public static readonly DependencyProperty SelectedItemsProperty =
+            DependencyProperty.RegisterAttached("SelectedItems", 
+                typeof(ObservableCollection<object>), 
+                typeof(AttachedProperties), 
+                new PropertyMetadata(null, OnPropertyChanged));
+
+        private static void OnPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            
+        }
+
+        public static ObservableCollection<object> GetSelectedItems(ListBox obj)
+        {
+            return (ObservableCollection<object>)obj.GetValue(SelectedItemsProperty);
+        }
+
+        public static void SetSelectedItems(DependencyObject obj, ObservableCollection<object> value)
+        {
+            obj.SetValue(SelectedItemsProperty, value);
+        }
+
+
+
+
+        #endregion
+    }
+}
