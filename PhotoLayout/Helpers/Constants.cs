77dcb7017ae6d22eb424a2284b1a448a2870f965
Nikola Milovanovic <nikola.milovanovic@easygoing.company>
2016-04-25 16:24:34 +0200

    Byte arrays (Layout matrices) added to Constants

--- PhotoLayout/Helpers/Constants.cs
+++ PhotoLayout/Helpers/Constants.cs
@@ -8,7 +8,29 @@ namespace PhotoLayout.Helpers
 {
     public static class Constants
     {
-        // Number of photos that can be selected from the collection and added to the LayoutGrid
+        // Number of photos that can be selected from the collection and added to the LayoutGrid   
+        // ***** If this value changes the Layout matrices below also need to be updated *******
         public const int MaxSelectedPhotos = 8;
+
+        /// <summary>
+        /// Represents a layout that consists only of rows.
+        /// </summary>
+        public static readonly byte[] JustRowsLayout = new byte[] { MaxSelectedPhotos, 0 };
+
+        /// <summary>
+        /// Represents a layout that consists only of columns.
+        /// </summary>
+        public static readonly byte[] JustColumnsLayout = new byte[] { 0, MaxSelectedPhotos };
+
+        /// <summary>
+        /// Represents a layout that can have 3 rows and 3 columns (max 9 photos).
+        /// </summary>
+        public static readonly byte[] ThreeByThreeLayout = new byte[] { 3, 3 };
+
+        /// <summary>
+        /// Represents a layout that can have 5 rows and 2 columns (max 10 photos).
+        /// </summary>
+        public static readonly byte[] FiveByTwoLayout = new byte[] { 5, 2 };
+
     }
 }
