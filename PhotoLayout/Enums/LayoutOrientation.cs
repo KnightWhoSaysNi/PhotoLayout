c0397024d71a594fafb88a47a55d1b83e4aa6cc3
KnightWhoSaysNi <nnikola.milovanovic@gmail.com>
2016-04-25 16:14:29 +0200

    LayoutAction and LayoutOrientation enums added

---
+++ PhotoLayout/Enums/LayoutOrientation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoLayout.Enums
+{
+    public enum LayoutOrientation
+    {
+        /// <summary>
+        /// On new photo new row is added until a limit is reached and only at that point is the new column added.
+        /// </summary>
+        RowFirst,
+
+        /// <summary>
+        /// On new photo new column is added until a limit is reached and only at that point is the new row added.
+        /// </summary>
+        ColumnFirst
+    }
+}
