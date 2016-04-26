93a019513f5a04a31c874de2703fbcb8fc1b2a0a
KnightWhoSaysNi <nnikola.milovanovic@gmail.com>
2016-04-26 12:50:26 +0200

    IntToVisibilityConverter added; SelectedItemCountToColorConverter renamed to IntToColor

---
+++ PhotoLayout/Converters/IntToColorConverter.cs
@@ -0,0 +1,37 @@
+﻿using PhotoLayout.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace PhotoLayout.Converters
+{
+    public class IntToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int count = (int)value;
+
+            if (count < Constants.MaxSelectedPhotos)
+            {
+                var brush = (SolidColorBrush)parameter;
+                return brush;
+            }
+            else
+            {
+                return Brushes.Red;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+    }
+}
